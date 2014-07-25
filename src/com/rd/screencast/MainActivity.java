84774564993fed481b7e4ab88e753995dbe6997e
thahiti <thahiti@gmail.com>
2014-07-25 16:20:35 +0900

    nothing big.

--- src/com/rd/screencast/MainActivity.java
+++ src/com/rd/screencast/MainActivity.java
@@ -31,15 +31,11 @@ public class MainActivity extends Activity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//				setContentView(R.layout.activity_main);
-//		
-//				if (savedInstanceState == null) {
-//					getFragmentManager().beginTransaction()
-//							.add(R.id.container, new PlaceholderFragment()).commit();
-//				}
-//				
+
 		mGLView = new MyGLSurfaceView(this);
 		setContentView(mGLView);
+		
+		
 	} 
 
 	public boolean onCreateOptionsMenu(Menu menu) {
